2aa4f6678089ce1e77d9b2488df162722e0e9887
Samuel Heluany <samuelheluany@hotmail.com>
2025-07-31 09:37:12 -0300

    saving data input with useState, data manipulation inline and with function, learn

--- 5_FORM_REACT/form_react/src/componnents/MyForm.jsx
+++ 5_FORM_REACT/form_react/src/componnents/MyForm.jsx
@@ -1,13 +1,29 @@
 import './MyForm.css'
 
+import { useState } from 'react'
+
 const MyForm = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+    console.log(email)
   return (
     <div>
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name='name' placeholder='Digite seu nome'/>
+                <input type="text" name='name' placeholder='Digite seu nome' onChange={handleName}/>
             </div>
+
+            <label>
+                <span>Email:</span>
+                <input type="email" name="email" placeholder='Digite o email' onChange={(e) => {
+                    setEmail(e.target.value)
+                }}/>
+            </label>
             <input type="submit" value="Enviar" />
         </form>
     </div>
